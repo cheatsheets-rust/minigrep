447bfdfd8773b2596d838c1bb5bc887124c8e937
Julien Chapron <jc271200001@protonmail.com>
2022-07-18 16:54:08 +0200

    add constrctor

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,33 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+
+    let config = Config::new(&args);
+
+    println!("On recherche : {}", config.recherche);
+    println!("Dans le fichier : {}", config.nom_fichier);
+
+    let contenu = fs::read_to_string(config.nom_fichier)
+        .expect("Quelque chose s'est mal passé lors de la lecture du fichier");
+
+    println!("Dans le texte :\n{}", contenu);
+}
+
+struct Config {
+    recherche: String,
+    nom_fichier: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        if args.len() < 3 {
+            panic!("il n'y a pas assez d'arguments");
+        }
+        let recherche = args[1].clone();
+        let nom_fichier = args[2].clone();
+
+        Config { recherche, nom_fichier }
+    }
 }
