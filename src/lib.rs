ccddb8e2906a5292ba7f0fab9567ea2c574106cb
Julien Chapron <jc271200001@protonmail.com>
2022-07-19 11:15:36 +0200

    add unit testung and output error

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,16 @@
+use std::env;
+
 use std::error::Error;
 use std::fs;
 
 pub struct Config {
     pub recherche: String,
     pub nom_fichier: String,
+    pub sensible_casse: bool,
 }
 
 impl Config {
     pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        // -- partie masquée ici --
         if args.len() < 3 {
             return Err("il n'y a pas assez d'arguments");
         }
@@ -16,21 +18,58 @@ impl Config {
         let recherche = args[1].clone();
         let nom_fichier = args[2].clone();
 
-        Ok(Config { recherche, nom_fichier })
+        let sensible_casse = env::var("CASE_INSENSITIVE").is_err();
+
+        Ok(Config {
+            recherche,
+            nom_fichier,
+            sensible_casse,
+        })
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    // -- partie masquée ici --
     let contenu = fs::read_to_string(config.nom_fichier)?;
 
-    println!("Dans le texte :\n{}", contenu);
+    let resultats = if config.sensible_casse {
+        rechercher(&config.recherche, &contenu)
+    } else {
+        rechercher_insensible_casse(&config.recherche, &contenu)
+    };
+
+    for ligne in resultats {
+        println!("{}", ligne);
+    }
 
     Ok(())
 }
 
 pub fn rechercher<'a>(recherche: &str, contenu: &'a str) -> Vec<&'a str> {
-    vec![]
+    let mut resultats = Vec::new();
+
+    for ligne in contenu.lines() {
+        if ligne.contains(recherche) {
+            resultats.push(ligne);
+        }
+    }
+
+    resultats
+}
+
+pub fn rechercher_insensible_casse<'a>(
+    recherche: &str,
+    contenu: &'a str,
+) -> Vec<&'a str> {
+    let recherche = recherche.to_lowercase();
+    let mut resultats = Vec::new();
+
+    for ligne in contenu.lines() {
+        if ligne.to_lowercase().contains(&recherche) {
+            resultats.push(ligne);
+        }
+    }
+
+    resultats
 }
 
 #[cfg(test)]
@@ -38,16 +77,29 @@ mod tests {
     use super::*;
 
     #[test]
-    fn un_resultat() {
+    fn sensible_casse() {
         let recherche = "duct";
         let contenu = "\
 Rust:
-sécurité, rapidité, productivité.
-Obtenez les trois en même temps.";
+safe, fast, productive.
+Pick three.
+Duct tape.";
+
+        assert_eq!(vec!["safe, fast, productive."], rechercher(recherche, contenu));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let recherche = "rUsT";
+        let contenu = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
 
         assert_eq!(
-            vec!["sécurité, rapidité, productivité."],
-            rechercher(recherche, contenu)
+            vec!["Rust:", "Trust me."],
+            rechercher_insensible_casse(recherche, contenu)
         );
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,15 @@ use std::process;
 use minigrep::Config;
 
 fn main() {
-    // -- partie masquée ici --
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problème rencontré lors de l'interprétation des arguments : {}", err);
+        eprintln!("Problème rencontré lors de l'interprétation des arguments : {}", err);
         process::exit(1);
     });
 
-    println!("On recherche : {}", config.recherche);
-    println!("Dans le fichier : {}", config.nom_fichier);
-
     if let Err(e) = minigrep::run(config) {
-        // -- partie masquée ici --
-        println!("Erreur applicative : {}", e);
+        eprintln!("Erreur applicative : {}", e);
 
         process::exit(1);
     }
